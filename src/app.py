0f9bc8daf247b75a42f88bf07c98cd6362e05eea
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2023-10-17 23:52:09 +0530

    Flask app created.

---
+++ src/app.py
@@ -0,0 +1,21 @@
+import re
+from flask import Flask, redirect, render_template, request,url_for
+from aprspass import aprspass
+
+"""
+This module provides a Flask web application that generates APRS passcodes.
+"""
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        call=request.form.get('call_sign')
+        passcode = aprspass(call)
+        return render_template('result.html', call=call, passcode=passcode)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/static/script.js
@@ -0,0 +1,3 @@
+const inputField = document.getElementById("callsign");
+const inputValue = inputField.value;
+console.log(inputValue); // Outputs the value of the input field
