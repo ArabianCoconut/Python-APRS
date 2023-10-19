d353bf0eaad752658d82e6246a2e5301375cd76c
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2023-10-19 13:39:15 +0530

    Added docs for function

--- src/app.py
+++ src/app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    Renders the index.html template and generates an APRS passcode if a POST request is received.
+
+    Returns:
+        If a POST request is received, returns the rendered index.html template with the generated APRS passcode.
+        Otherwise, returns the rendered index.html template without a passcode.
+    """
     if request.method == 'POST':
         call=request.form.get('call_sign')
         passcode = aprspass(call)
